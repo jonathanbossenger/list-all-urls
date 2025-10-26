2aeeccfbe3a5287b7e5b8c1c88d04c84df64f517
Jonathan Bossenger <jonathanbossenger@gmail.com>
2025-10-26 15:07:18 +0200

    Refactoring plugin to use an Ability
    PHP refactor

--- list-all-urls.php
+++ list-all-urls.php
@@ -38,24 +38,79 @@ function list_all_urls_get_all_post_types(): array {
 	return get_post_types( $args, $output, $operator );
 }
 
+add_action( 'abilities_api_categories_init', 'list_all_urls_register_ability_categories' );
 /**
- * Generate a list of URLs based on the provided arguments
+ * Register the ability category for the plugin
+ *
+ * @return void
+ */
+function list_all_urls_register_ability_categories() {
+    wp_register_ability_category( 'list-all-urls', array(
+            'label' => __( 'List all URLs', 'list-all-urls' ),
+            'description' => __( 'Abilities from the List all URLs plugin.', 'list-all-urls' ),
+    ));
+}
+
+add_action( 'abilities_api_init', 'list_all_urls_register_abilities' );
+function list_all_urls_register_abilities() {
+    wp_register_ability(
+        'list-all-urls/urls',
+        array(
+            'label' => __( 'Get All URLs', 'list-all-urls' ),
+            'description' => __( 'Retrieves a list of URLs from the WordPress site, optionally as clickable anchor links.', 'list-all-urls' ),
+            'category' => 'list-all-urls',
+            'input_schema' => array(
+                'type' => 'object',
+                'properties' => array(
+                        'post_type' => array(
+                                'type' => 'string',
+                                'description' => 'The post type to retrieve URLs from (e.g., post, page, custom post type).',
+                            ),
+                        'posts_per_page' => array(
+                                'type' => 'boolean',
+                                'description' => 'Number of posts to retrieve. Use -1 to retrieve all posts.',
+                        ),
+                        'post_status' => array(
+                                'type' => 'boolean',
+                                'description' => 'The status of the posts to retrieve (e.g., publish, draft).',
+                        ),
+                        'makelinks' => array(
+                                'type' => 'boolean',
+                                'description' => 'Whether to return URLs as clickable anchor links.',
+                        ),
+                ),
+            ),
+            'output_schema' => array(
+                    'type' => 'object',
+                    'properties' => array(
+                            'url' => array(
+                                    'type' => 'string',
+                                    'description' => 'URL or clickable link to the URL'
+                            )
+                    )
+            ),
+            'execute_callback' => 'list_all_urls_generate_url_list',
+            'permission_callback' => '__return_true',
+        )
+    );
+}
+
+/**
+ * Generate a list of URLs based on the provided input
  * Optionally make them clickable links
  *
- * @param array $arguments Arguments to customize the URL generation.
- * @param bool  $makelinks Whether to return clickable links or plain URLs (escaped).
+ * @param array $input The Abilities API input arguments.
  *
  * @return array List of generated URLs.
  */
-function list_all_urls_generate_url_list( array $arguments = array(), bool $makelinks = false ): array {
-    $default_args   = array(
-            'post_type'      => 'post',
-            'posts_per_page' => - 1,
-            'post_status'    => 'publish',
+function list_all_urls_generate_url_list( $input ): array {
+    $args = array(
+            'post_type'      => isset( $input['post_type'] ) ? sanitize_text_field( $input['post_type'] ) : 'post',
+            'posts_per_page' => isset( $input['posts_per_page'] ) ? intval( $input['posts_per_page'] ) : -1,
+            'post_status'    => isset( $input['post_status'] ) ? sanitize_text_field( $input['post_status'] ) : 'publish',
     );
-    $args           = wp_parse_args( $arguments, $default_args );
     $posts          = get_posts( $args );
-
+    $makelinks      = isset( $input['makelinks'] ) && $input['makelinks'] ;
     $links = array();
     foreach ( $posts as $post ) {
         $permalink = get_permalink( $post );
@@ -79,37 +134,6 @@ function list_all_urls_blocks_init() {
 	wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
 }
 
-add_action( 'rest_api_init', 'list_all_urls_register_rest_route' );
-function list_all_urls_register_rest_route (): void {
-    register_rest_route(
-            'list-all-urls/v1',
-            '/urls',
-            array(
-                'methods' => 'GET',
-                'callback' => 'list_all_urls_rest_fetch_all_urls',
-                'args' => array(
-                        'type' => array(
-                                'validate_callback' => function( $param ) {
-                                    return is_string( $param );
-                                }
-                        ),
-                ),
-            )
-    );
-}
-
-function list_all_urls_rest_fetch_all_urls( $arguments ){
-    if ( isset($arguments['type'] ) ) {
-        $post_type = sanitize_text_field( wp_unslash( $arguments['type'] ) );
-    } else {
-        $post_type = 'any';
-    }
-    $args = array(
-        'post_type'      => $post_type,
-    );
-    return list_all_urls_generate_url_list( $args );
-}
-
 add_action( 'admin_menu', 'list_all_urls_plugin_menu' );
 /**
  * Add plugin menu to the WordPress admin dashboard via the Tools menu
@@ -131,7 +155,7 @@ function list_all_urls_render_admin_page() {
 	if ( ! current_user_can( 'manage_options' ) ) {
 		wp_die( esc_html__( 'You do not have sufficient permissions to access this page.', 'list-all-urls' ) );
 	}
-	$post_types = get_all_post_types();
+	$post_types = list_all_urls_get_all_post_types();
 	?>
 
 	<div class="wrap">
@@ -185,13 +209,14 @@ function list_all_urls_render_admin_page() {
 				$makelinks = true;
 			}
 
-			$args = array(
+			$input = array(
 				'post_type'      => $post_type,
 				'posts_per_page' => - 1,
 				'post_status'    => 'publish',
+                'makelinks'      => $makelinks,
 			);
 
-			$links = list_all_urls_generate_url_list( $args, $makelinks );
+			$links = list_all_urls_generate_url_list( $input );
 
 			if ( $links ) {
 				echo '<p><strong>Below is a list of your requested URLs:</strong></p>';
